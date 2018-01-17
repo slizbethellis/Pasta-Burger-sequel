dba443b020425ede2b59058c44aeb6640de4f023
slizbethellis <slizbethellis@gmail.com>
2018-01-17 16:21:42 -0600

    removed commented out route and unused variable

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -23,19 +23,10 @@ router.post("/api/burgers", function(req, res) {
   });
 });
 
-//router.post("/api/customers", function(req, res) {
-//  db.Customer.create(req.body).then(function(dbPost) {
-//    res.json(dbPost);
-//    return dbPost.dataValues.id;
-//  });
-//});
-
 router.put("/api/burgers/:id", function(req, res) {
-  var custId;
-  db.Customer.create(req.body).then(function(dbPost) {
-    custId = dbPost.dataValues.id;
-  }).then(db.Burger.update(
-    {devoured: true},
+  db.Customer.create(req.body).then(function(dbPost) {}
+    ).then(db.Burger.update(
+      {devoured: true},
     {
       where: {
         id: req.params.id
